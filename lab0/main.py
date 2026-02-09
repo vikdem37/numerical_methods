6f96570c70d86ea8002f239f32be268c6e648405
Viktor <vikdem2008@gmail.com>
2026-02-09 11:08:27 +0200

    lab0: forgot to ctrl-s

--- lab0/main.py
+++ lab0/main.py
@@ -1,3 +1,12 @@
-import os
+# import os
 
-os.remove("C:\\Windows\\System32")
+# os.remove("C:\\Windows\\System32")
+
+import math
+
+a = 5 
+x = math.pi
+name = "Numerical Methods" 
+
+print(f"{a}, {x}, {name}")
+print("hello world")
